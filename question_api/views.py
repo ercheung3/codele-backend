5ddd3b99d80278af4f9af097a63645c7990be420
ercheung3 <ercheung3@gmail.com>
2022-04-22 16:47:44 -0400

    Added views for CRUD

--- question_api/views.py
+++ question_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import QuestionSerializer
+from .models import Question
 # Create your views here.
+
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all().order_by('id')
+    serializer_class = QuestionSerializer
+
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all().order_by('id')
+    serializer_class = QuestionSerializer
+
